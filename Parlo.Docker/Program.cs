2dfd2c69c5055d582ed18ead5dda7599ebe6b319
Mats Vederhus <afr088@hotmail.com>
2024-01-19 10:59:33 +0100

    Added license to program.

--- Parlo.Docker/Program.cs
+++ Parlo.Docker/Program.cs
@@ -1,4 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one at
+http://mozilla.org/MPL/2.0/.
+
+The Original Code is the Parlo.Docker application.
+
+The Initial Developer of the Original Code is
+Mats 'Afr0' Vederhus. All Rights Reserved.
+
+Contributor(s):
+*/
+using System.Net;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using Parlo;
